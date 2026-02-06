ddb7b23f6b4303e37fd57d3e0f8f03f3c575e79a
ChanduGowdaMS <chandugowda64959@gmail.com>
2026-02-06 12:59:10 +0530

    added

---
+++ hybrid.py
@@ -0,0 +1,33 @@
+class Person:
+    def __init__(self, name):
+        self.name = name
+    def display_person(self):
+        print("Name:", self.name)
+class Student(Person):
+    def __init__(self, name, student_id):
+        Person.__init__(self, name)
+        self.student_id = student_id
+
+    def display_student(self):
+        self.display_person()
+        print("Student ID:", self.student_id)
+class SportsPlayer(Person):
+    def __init__(self, name, sport_name):
+        Person.__init__(self, name)
+        self.sport_name = sport_name
+
+    def display_sports_player(self):
+        self.display_person()
+        print("Sport Name:", self.sport_name)
+class CollegeStudent(Student, SportsPlayer):
+    def __init__(self, name, student_id, sport_name, college_name):
+        Student.__init__(self, name, student_id)
+        SportsPlayer.__init__(self, name, sport_name)
+        self.college_name = college_name
+
+    def display_college_student(self):
+        self.display_student()
+        self.display_sports_player()
+        print("College Name:", self.college_name)
+obj=CollegeStudent("chandu", 1, "cricket", "bgs")
+obj.display_college_student()
